b60a1c253e0cda0b92055f271a7ba5008515d4f7
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-06-21 11:55:33 +0200

    functions that returns a value test

---
+++ js/17.js
@@ -0,0 +1,13 @@
+// Functions that returns a value
+
+const sum = (num1 = 0, num2 = 0) => num1 + num2
+
+// const sum = (num1 = 0, num2 = 0) => {
+//     return num1 + num2
+// }
+
+const result1 = sum(10, 20)
+sum(100, 203)
+sum(10)
+
+console.log(result1)
