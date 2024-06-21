a5c905ec388c4062139c212c530412b9b9c7d53c
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-06-21 11:47:57 +0200

    arrow function test

---
+++ js/16.js
@@ -0,0 +1,7 @@
+// Arrow Functions, only can be used when its a function expression and MUST be declared before
+
+const sum = (num1 = 0, num2 = 0) => console.log(num1 + num2)
+
+sum(10, 20)
+sum(100, 203)
+sum(10)
