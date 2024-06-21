9961896730c06c1a8a3f6b8723d78bb87028d084
Sobdev <71671270+Sobdev@users.noreply.github.com>
2024-06-21 11:05:29 +0200

    using for test

--- js/11.js
+++ js/11.js
@@ -2,6 +2,8 @@ const tech = ['HTML', 'CSS', 'JAVASCRIPT', 'REACT.JS', 'NODE.JS']
 
 const react = tech[3]
 console.log(react)
+
 const [html, css, nodejs] = tech
+
 console.log(nodejs)
 
---
+++ js/12.js
@@ -0,0 +1,14 @@
+const tech = ['HTML', 'CSS', 'JAVASCRIPT', 'REACT.JS', 'NODE.JS']
+
+// Initial value of the index ; do it till i is less than 5... ; ingrease
+
+for (let i = 0; i < 5; i++) {
+    console.log(tech[i])
+
+}
+
+// Initial value of the index ; do it till i is less than tech length... ; ingrease
+
+for (let i = 0; i < tech.length; i++) {
+    console.log(tech[i])
+}
